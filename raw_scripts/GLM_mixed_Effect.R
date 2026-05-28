25eb07c43ef609bc8bef7bcbf6f3026d6948c9b0
Dennis Leung <43949365+dmhleung@users.noreply.github.com>
2026-05-28 12:24:52 +1000

    expositional changes to wine data analysis
    Also fixed up a typo in the raw script

--- raw_scripts/GLM_mixed_Effect.R
+++ raw_scripts/GLM_mixed_Effect.R
@@ -11,7 +11,7 @@
 #  mcemGLM  (MCMC+ EM)
 
 # library(MASS)
-# 
+#
 library(lme4)
 # library(mcemGLM)
 library(geepack)  # for the ohio dataset
@@ -26,7 +26,7 @@ ohio$smoke= as.factor(ohio$smoke)
 contrasts(ohio$age)
 contrasts(ohio$smoke)
 # F and T uses effect coding in the example so we need to set the contrasts here
-options(contrasts = c("contr.sum", "contr.poly"))   # set the contrasts for the covariates to be 
+options(contrasts = c("contr.sum", "contr.poly"))   # set the contrasts for the covariates to be
 contrasts(ohio$age)
 contrasts(ohio$smoke)
 #contrast for smoke status needs to be further adjusted
@@ -37,18 +37,18 @@ contrasts(ohio$smoke) = matrix(c(-1, 1), 2, 1)
 
 
 
-glmer_fit = lme4::glmer(resp ~  age*smoke     + 
+glmer_fit = lme4::glmer(resp ~  age*smoke     +
                           (1|id), data = ohio, family = binomial,
-                        control = glmerControl(optimizer ="Nelder_Mead"))  
+                        control = glmerControl(optimizer ="Nelder_Mead"))
 # the "default" recommended by Faraway (2016)'s book
 
 summary(glmer_fit )
 
 ## the following can closely reproduce the numbers in table 7.2 of F&T for Gauss-Hermite with 10 quadrature points
-glmer_fit_nAGQ_10 = lme4::glmer(resp ~  age*smoke     + 
+glmer_fit_nAGQ_10 = lme4::glmer(resp ~  age*smoke     +
                           (1|id), data = ohio, family = binomial,
                         control = glmerControl(optimizer ="Nelder_Mead"),
-                        nAGQ = 10) 
+                        nAGQ = 10)
 
 
 summary(glmer_fit_nAGQ_10 )
@@ -78,7 +78,7 @@ head(ranef(glmmPQL_fit)  )# this should be the version of ranef() in nlme
 
 library(ordinal)
 library(MASS)
-options(contrasts = c("contr.treatment", "contr.poly"))  
+options(contrasts = c("contr.treatment", "contr.poly"))
 head(wine)  # this is from the ordinal package
 
 
@@ -104,7 +104,7 @@ wine_fixed_fit_polr =  polr(rating~temp + contact, data=wine, Hess=T,method="log
 summary(wine_fixed_fit_polr  )
 
 
-#  we will ak=lso try the function in the ordinal package
+#  we will also try the function in the ordinal package
 wine_fixed_fit_clm <-  clm(rating ~ temp +contact, data = wine) # another fixed effect fit with the "ordinal" package
 summary(wine_fixed_fit_clm)
 wine_fixed_fit_clm$coefficients
@@ -112,7 +112,7 @@ wine_fixed_fit_clm$coefficients
 
 # for comparison, I computed these thresholds reported in the book
 book_coeff = c(
--5.289, 
+-5.289,
 -5.289 + exp(0.974),
 -5.289 + exp(0.974) + exp(0.739),
 -5.289 + exp(0.974) + exp(0.739) + exp(0.426)
